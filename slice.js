32481f4e41f6de5144db5c9f3baf0d29ff42241e
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-06-01 17:59:02 +0500

    Add files via upload

---
+++ slice.js
@@ -0,0 +1,4 @@
+"use strict";
+let makeupKit = ["eyeshadow palette", "Blushon Kit", "Lipsticks", "Nail polish"];
+let newmakeupKit = makeupKit.slice(0, 3);
+console.log(newmakeupKit);
---
+++ slice.ts
@@ -0,0 +1,3 @@
+let makeupKit:string[]=["eyeshadow palette", "Blushon Kit", "Lipsticks", "Nail polish"]
+let newmakeupKit:string[]= makeupKit.slice(0,3);
+console.log(newmakeupKit);
